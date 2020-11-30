65000bf5c092d9b16c357e9d9abcc73dd5bbb8f8
fajarrasyid <72869725+fajarrasyid@users.noreply.github.com>
2020-11-30 13:04:39 +0700

    Pembuatan Aktivitas

---
+++ app/src/main/java/com/kolot/x_servicesbeta/BuatPesanan.kt
@@ -0,0 +1,11 @@
+package com.kolot.x_servicesbeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuatPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buat_pesanan)
+    }
+}
---
+++ app/src/main/java/com/kolot/x_servicesbeta/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.kolot.x_servicesbeta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        BtnPesan.setOnClickListener{
+            val intent = Intent(this, Pesan::class.java)
+            startActivity(intent)
+        }
+        BtnPengaturan.setOnClickListener{
+            val intent = Intent(this, Pengaturan::class.java)
+            startActivity(intent)
+        }
+        BtnHome.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        BtnBerita.setOnClickListener{
+            val intent = Intent(this, Berita::class.java)
+            startActivity(intent)
+        }
+        BtnPemberitahuan.setOnClickListener{
+            val intent = Intent(this, Pemberitahuan::class.java)
+            startActivity(intent)
+        }
+        BtnBuat.setOnClickListener{
+            val intent = Intent(this, Pemberitahuan::class.java)
+            startActivity(intent)
+        }
+    }
+}
