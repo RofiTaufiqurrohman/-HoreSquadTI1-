1868ccab43c96f8cc1aa9e50ee3c45484fbd82a4
fajarrasyid <72869725+fajarrasyid@users.noreply.github.com>
2020-12-01 21:01:57 +0700

    penyesuaian

--- app/src/main/java/com/kolot/x_servicesbeta/Tentang.kt
+++ app/src/main/java/com/kolot/x_servicesbeta/Tentang.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_tentang.*
 
 class Tentang : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tentang)
-        BtnPesan.setOnClickListener{
-            val intent = Intent(this, Pesan::class.java)
+        maps.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
     }
